3e28375bb1b841c89389e15ba0699acd89a03dbb
keepflowing <132106900+keepflowing@users.noreply.github.com>
2023-10-13 17:10:19 -0500

    Write test to see all possible moves from a square

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 import Board from './Board.js';
+import Knight from './Knight.js';
 
 const board = new Board;
+const knight = new Knight;
+
 board.init();
+
+board.squares[0][0] = 'x';
+
+const node = knight.createTree();
+console.log(node);
+for (let i = 0; i < node.nextMoves.length; i++) {
+  const a = node.nextMoves[i][0];
+  const b = node.nextMoves[i][1];
+  console.log(a, b);
+  board.squares[a][b] = 'o';
+}
 
 board.draw();
 // console.log(board.squares);
